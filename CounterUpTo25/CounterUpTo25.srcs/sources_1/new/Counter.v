b05ec75fd20142ccce93c8f1be0b6fe2afd51715
eozbademci <eozbademci@gmail.com>
2022-04-05 21:53:28 +0300

    Counter

---
+++ CounterUpTo25/CounterUpTo25.srcs/sources_1/new/Counter.v
@@ -0,0 +1,27 @@
+module sayici(
+    input clk,rst,
+    output wire[4:0] Q
+    );
+    
+    reg[4:0] outreg=5'b00000;
+    
+    parameter MAX_VALUE = 5'b11001;
+    
+    assign Q = outreg;
+    
+    always @(posedge clk)
+    begin
+    if(rst) begin
+    outreg = 0;
+    end
+    else begin
+    if(outreg >= MAX_VALUE) begin
+    outreg = 0;       
+    end
+    else begin
+    outreg = outreg + 1;
+    end
+    end
+    end
+    
+endmodule
