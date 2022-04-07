a7daeb8095857c392290c650decebb10158f74d6
eozbademci <eozbademci@gmail.com>
2022-04-07 20:05:56 +0300

    Create jk_flipflop.v

---
+++ FlipFlop/jk_flipflop.v
@@ -0,0 +1,21 @@
+module jk_flipflop(
+    input wire j,
+    input wire k,
+    input clk,
+    output reg Q 
+    );
+    
+    
+always @(posedge clk)
+
+begin
+case({j,k})
+
+2'b00: Q <= Q;
+2'b01: Q <= 0;
+2'b10: Q <= 1;
+2'b11: Q <= ~Q;
+
+endcase
+end
+endmodule
